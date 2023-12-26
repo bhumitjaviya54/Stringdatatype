c9297af4895dca462f2fbc59042e5495d9410ddf
bhumitjaviya54 <93394436+bhumitjaviya54@users.noreply.github.com>
2023-12-26 16:26:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// String Data Type
+
+// In JS, a string is a sequence of zero or more characters enclosed in single or double quotes.
+
+// JS string are storing and manipulating text.
+
+let firstName = "Hello"; // double quote
+let lastName = "world"; // single quote
+
+console.log(firstName);
+console.log(lastName);
